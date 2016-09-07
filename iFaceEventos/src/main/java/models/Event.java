bad11e5687d5cf910f3fe9ddc4d64718b0bb3dac
Vanessa <vsv@ic.ufal.br>
2016-09-07 19:04:05 -0300

    events are persisting

--- iFaceEventos/src/main/java/models/Event.java
+++ iFaceEventos/src/main/java/models/Event.java
@@ -3,6 +3,7 @@
  */
 package models;
 
+import java.util.ArrayList;
 /**
  * @author Vinicius
  *
@@ -29,11 +30,14 @@ public class Event {
 	@Column
 	private String description;
 	@Column
-	private Date date;
+	private String date;
+	@Column
+	private String hour;
 	
 	@ManyToMany
-	@JoinTable(name = "user_event", joinColumns = @JoinColumn(name = "event_id"), inverseJoinColumns = @JoinColumn(name = "user_d"))
-	protected List<User> guests;
+	@JoinTable(name = "user_event", joinColumns = @JoinColumn(name = "event_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+	protected List<User> guests = new ArrayList<User>();
+;
 
 	@ManyToOne
 	protected User host;
@@ -41,11 +45,12 @@ public class Event {
 		public Event() {
 	}
 	
-	public Event(String name, String description, User host, Date date) {
+	public Event(String name, String description, User host, String date, String hour) {
 		this.name = name;
 		this.description = description;
 		this.host = host;
 		this.date = date;
+		this.hour = hour;
 	}
 
 	public int getId() {
@@ -80,13 +85,22 @@ public class Event {
 		this.description = description;
 	}
 
-	public Date getDate() {
+	public String getDate() {
 		return date;
 	}
 
-	public void setDate(Date date) {
+	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	public String getHour() {
+		return hour;
+	}
+
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+
 
 	public List<User> getGuests() {
 		return guests;
@@ -102,6 +116,14 @@ public class Event {
 
 	public void setHost(User host) {
 		this.host = host;
+	}
+	
+	public void addGuests (User user){
+		this.guests.add(user);
+	}
+	
+	public void removeGuestsEvents (User user){
+		this.guests.remove(user);
 	}
 
 }
