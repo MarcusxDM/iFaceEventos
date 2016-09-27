5f148e734e5a0a1bd37460411d8bdd08afb7a33e
marcusxdm <marcuspestana@ic.ufal.br>
2016-09-27 19:19:54 -0300

    Revert "Revert "Deleting Duplicates""
    This reverts commit 20961183ab6c3dad7fa3c918dcf220a8b59d2c3c.

--- src/main/java/exceptionsFile/EventAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package exceptionsFile;
-
-public class EventAlreadyExistsException extends Exception {
-	public EventAlreadyExistsException() {
-		super();
-	} 
-	  public EventAlreadyExistsException(String message) { super(message); }
-}
